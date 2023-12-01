bcdf7f57549ab824c0b574afed27211a54a429a5
vishalsamanta123 <vishalitinformatix@gmail.com>
2023-12-01 12:38:20 +0530

    Updation on android completed

--- android/app/src/main/java/com/hdfc/MainActivity.java
+++ android/app/src/main/java/com/hdfc/MainActivity.java
@@ -2,6 +2,9 @@ package com.audit.Kreate;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
 
@@ -19,4 +22,21 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
    }
+
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+        );
+  }
 }
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: ['react-native-reanimated/plugin',
+  '@babel/plugin-transform-arrow-functions',
+  '@babel/plugin-proposal-optional-chaining',
+  "@babel/plugin-proposal-nullish-coalescing-operator",
+  "@babel/plugin-transform-template-literals",
+   ],
 };
